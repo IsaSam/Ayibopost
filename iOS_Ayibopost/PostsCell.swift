eb32a59c5c56b9c3f8ef4bbe6828d056944b7417
IsaSam <isaacsamuel27@gmail.com>
2019-01-17 13:21:16 -0500

    Post receive Json data

--- iOS_Ayibopost/PostsCell.swift
+++ iOS_Ayibopost/PostsCell.swift
@@ -13,6 +13,7 @@ class PostsCell: UITableViewCell {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var contentLabel: UILabel!
     
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
