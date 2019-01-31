1aafb39ec09ecdc1ff30e8414ecd41f42257b9fc
IsaSam <isaacsamuel27@gmail.com>
2019-01-31 15:35:28 -0500

    framework drawer added

---
+++ iOS_Ayibopost/ViewController1.swift
@@ -0,0 +1,98 @@
+//
+//  ViewController.swift
+//  AyiboPostIos
+//
+//  Created by Jules Frantz Stephane Loubeau on 10/30/18.
+//  Copyright © 2018 Jules Frantz Stephane Loubeau. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import AlamofireImage
+import SwiftyJSON
+
+class ViewController1: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    var posts: [[String: Any]] = []
+    var imgPosts: [[String: Any]] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.rowHeight = 168
+        tableView.estimatedRowHeight = 200
+        
+        getPostList()
+    }
+    private func getPostList(){
+        AyiboAPIManager.shared.get(url: "https://ayibopost.com/wp-json/posts/") { (result, error) in
+            
+            if error != nil{
+                print(error!)
+                return
+            }
+            //print(result!)
+            self.posts = result!
+            self.tableView.reloadData() // to tell table about new data
+        }
+
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+        
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostsCell", for: indexPath) as! PostsCell
+        
+        let post = posts[indexPath.row]
+        cell.titleLabel.text = post["title"] as? String
+        let htmlTag = post["content"] as! String
+        let content = htmlTag.replacingOccurrences(of: "<[^>]+>", with: "", options: .regularExpression, range: nil)
+        cell.contentLabel.text = content
+        
+        do{
+            let imgArray = (posts as AnyObject).value(forKey: "featured_image")
+            let dataDic = imgArray as? [[String: Any]]
+            self.imgPosts = dataDic!
+            
+            let remoteImageUrlString = imgPosts[indexPath.row]
+            let imageURL = remoteImageUrlString["source"] as? String
+            print(imageURL!)
+            if let imagePath = imageURL,
+                let imgUrl = URL(string:  imagePath){
+                cell.imagePost.af_setImage(withURL: imgUrl)
+            }
+            else{
+                cell.imagePost.image = nil
+            }
+        }
+
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)
+        let post = posts[(indexPath?.row)!]
+        let imgPost = imgPosts[(indexPath?.row)!]
+        let detailViewController = segue.destination as! DetailsPostViewController
+        detailViewController.post = post
+        detailViewController.imgPost = imgPost
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
