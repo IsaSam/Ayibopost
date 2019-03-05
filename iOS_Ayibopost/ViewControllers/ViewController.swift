34041b02795d3d992c3e6de7969d859435d60b41
IsaSam <isaacsamuel27@gmail.com>
2019-03-04 21:18:05 -0500

    cleaning code

--- iOS_Ayibopost/ViewControllers/ViewController.swift
+++ iOS_Ayibopost/ViewControllers/ViewController.swift
@@ -12,20 +12,14 @@ import AlamofireImage
 import SwiftyJSON
 import SDWebImage
 
-//var defaults = UserDefaults(suiteName: "com.isasam.iOS-Ayibopost")
-//var defaults = UserDefaults.standard
-
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, DrawerControllerDelegate, UISearchBarDelegate, PostsCellDelegate {
     @IBOutlet weak var titleLogo: UIButton!
-    var delegate: BookmarkViewController!
-    
-    
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var activityIndicatory: UIActivityIndicatorView!
     @IBOutlet weak var searchBar: UISearchBar!
-    
     @IBOutlet weak var searchButton: UIBarButtonItem!
-  //  @IBOutlet weak var favButton: UIBarButtonItem!
+    
+    var delegate: BookmarkViewController!
     
     @IBAction func onTap(_ sender: Any) {
                 view.endEditing(true)
@@ -44,10 +38,8 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     var imgURLShare: String?
     var titleShare: String?
     var imgShare: UIImage?
- //   var favResults: [Post] = []
     var favResults: [[String: Any]] = []
     var favResults1: [[String: Any]] = []
-  //  var searchController = UISearchController()
     
     var idx: Int?
     var favClic: UIButton?
@@ -57,7 +49,6 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
      // -------------------------------
         // 1.Decllare the drawer view
         var drawerVw = DrawerView()
-        
         var vwBG = UIView()
     //--------------------
 
@@ -72,79 +63,29 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         navigationItem.leftBarButtonItem?.accessibilityElementsHidden = true
         navigationItem.rightBarButtonItem?.accessibilityElementsHidden = true
         searchBar.isHidden = false
-        //tap.cancelsTouchesInView = false
-       // searchButton.isEnabled = false
-     //   searchButton.accessibilityElementsHidden = true
-     //   favButton.accessibilityElementsHidden = true
-      //  navigationItem.rightBarButtonItem?.isEnabled = true
-    //    other()
-  
-   //     closeSearch.isEnabled = true
     }
     
-    /*
-    @IBAction func closeSearch(_ sender: UIBarButtonItem) {
-        searchBar.isHidden = true
-        closeSearch.isEnabled = false
-        searchBar.text = ""
- }
-    */
-
-
     @IBAction func addFav(_ sender: UIButton) {
         
-       // sender.isSelected = true
-   //     myButtonTapped()
-        
-         //   sender.setImage(UIImage(named: "FavYellow96"), for: .highlighted)
-        
-     /*
-        if f == false{
-            print(f)
-            self.favClicked?.setImage(UIImage(named: "FavYellow96"), for: .normal)
-            f = true
-        }else{
-            print(f)
-            self.favClic?.setImage(UIImage(named: "addFav100"), for: .normal)
-            f = false
-        }*/
- /*       if favClicked == true{
-         //   addFav.setImage(UIImage(named: "FavYellow96"), for: .normal)
-            favClicked = false
-            print("labas")
-        }
-        else{
-            cell.favButton.setImage(UIImage(named: "addFav100"), for: .normal)
-            favClicked = true
-            print("ici")
-        }
-        */
         print("Selected Item #\(sender.tag) as a favorite")
         favResults.append(posts[sender.tag])
-        
  //      print(favResults)
         print("Counter1: \(favResults.count)")
         self.favResults.reverse() //sort
         
-        storeData()
+        storeData() //Saved posts
         
         let alert = UIAlertController(title: "Post saved successfully!", message: "Read Later all Bookmark's 📖", preferredStyle: .alert)
         let okAction = UIAlertAction(title: "Continue", style: .default, handler: nil)
         alert.addAction(okAction)
         self.present(alert, animated: true, completion: nil)
-        
-
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        getData()
-    //    navigationItem.titleView = searchBar
-    //    searchBar.isHidden = true
-        
-        topBarLogo()
-   //     titleLogoImage()
         searchBar.delegate = self
+        getData() //get bookmarks
+        topBarLogo()
         
         self.refreshControl = UIRefreshControl()
         self.refreshControl.addTarget(self, action: #selector(ViewController.didPullToRefresh(_:)), for: .valueChanged)
@@ -152,29 +93,29 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         tableView.delegate = self
         tableView.rowHeight = 330
         tableView.estimatedRowHeight = 350
-        
         tableView.insertSubview(refreshControl, at: 0)
         tableView.dataSource = self
         
         getPostList()
+        
         //-----------------
         self.navigationController?.navigationBar.isTranslucent = false
         //-----------------
-        self.hideKeyboardOnTap(#selector(self.onTap(_:)))
         
+        self.hideKeyboardOnTap(#selector(self.onTap(_:)))
     }
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(true)
-        getData()
+        getData() // get posts
     }
     
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(true)
-        storeData()
+        storeData() // saved posts
     }
     
-    @objc func myButtonTapped(){
-       
+  /*  @objc func myButtonTapped(){
         if favClic?.isSelected == true {
             print("first")
             favClic?.isSelected = false
@@ -186,26 +127,10 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
             print("second")
             favClic?.isSelected = true
             favClic?.setImage(UIImage(named :" #imageLiteral(resourceName: fav)"), for: UIControlState.normal)
-            
-        //    favClic?.setImage(UIImage(named : "favYellow96"), for: UIControlState.normal)
-        //    favClic?.backgroundColor = UIColor.red
-     //       favClic?.colo
         }
- //       favClic?.addTarget(self, action: #selector(myButtonTapped), for: UIControlEvents.touchUpInside)
-        //     self.view.addSubview(favClic!)
-    }
-    func titleLogoImage(){
-        let logoContainer = UIView(frame: CGRect(x: 0, y: 0, width: 270, height: 30))
-        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 270, height: 30))
-        imageView.contentMode = .scaleAspectFit
-        let image = titleLogo.imageView
-    //    imageView.image = image
-        logoContainer.addSubview(image!)
-        navigationItem.titleView = logoContainer
-    }
+    }*/
     func topBarLogo(){
         let logoContainer = UIView(frame: CGRect(x: 0, y: 0, width: 270, height: 30))
-        
         let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 270, height: 30))
         imageView.contentMode = .scaleAspectFit
         let image = UIImage(named: "ayibopost-logo-blanc-2.png")
@@ -219,8 +144,6 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
         //-----------------
         @IBAction func actShowMenu(_ sender: Any) {
-            
-            //**** REQUIRED ****//
             //**** 2.Implement the drawer view object and set delecate to current view controller
             drawerVw = DrawerView(aryControllers:DrawerArray.array, isBlurEffect:true, isHeaderInTop:false, controller:self)
             drawerVw.delegate = self
@@ -244,56 +167,40 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
          AyiboAPIManager.shared.get(url: "https://ayibopost.com/wp-json/posts?page=\(loadNumber)") { (result, error) in
          
          if error != nil{
-        // print(error!)
                 let errorAlertController = UIAlertController(title: "Cannot Get Data", message: "The Internet connections appears to be offline", preferredStyle: .alert)
                 let cancelAction = UIAlertAction(title: "Retry", style: .cancel)
                 errorAlertController.addAction(cancelAction)
                 self.present(errorAlertController, animated: true)
-                //print(error!)
-
          return
          }
-         //print(result!)
          self.posts = result!
          self.tableView.reloadData() // to tell table about new data
          self.activityIndicatory.stopAnimating() //====================
          }
                 self.refreshControl.endRefreshing()
                 self.activityIndicatory.stopAnimating()
-   
     }
-    
     
     func loadMorePosts(){
       loadNumber = loadNumber + 1
       AyiboAPIManager.shared.get(url: "https://ayibopost.com/wp-json/posts?page=\(loadNumber)") { (result, error) in
             
                 if error != nil{
-                    // print(error!)
                     let errorAlertController = UIAlertController(title: "Cannot Get Data", message: "The Internet connections appears to be offline", preferredStyle: .alert)
                     let cancelAction = UIAlertAction(title: "Retry", style: .cancel)
                     errorAlertController.addAction(cancelAction)
                     self.present(errorAlertController, animated: true)
-                    //print(error!)
                     
                     return
                 }
-        
-                //print(result!)
-                //self.posts = result!
-        
             do{
                 for item in result!
                 {
-                    //self.dataList.add(item)
                     self.posts.append(item)
                 }
-                //print(result!)
                 self.tableView.reloadData() // to tell table about new data
             }
         }
-        
-        
     }
     
     func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
@@ -321,56 +228,19 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "PostsCell", for: indexPath) as! PostsCell
         let post = self.searchBar.text!.isEmpty ? posts[indexPath.row] : filteredPosts![indexPath.row]
-        
-//        let idx: Int = indexPath.row
         idx = indexPath.row
 //=====================================================================
-       // cell.favButton.tag = idx!
         cell.favButton.tag = indexPath.row
         
-        
-   //     favClic = cell.favButton
-        
-    /*    if cell.favButton.isSelected == false{
-            cell.favButton.tintColor = UIColor.yellow
-        }
-        else{
-            cell.favButton.tintColor = UIColor.green
-        }*/
-    /*
-        if favClic?.isSelected == true {
-            print("first 01")
-  //          favClic?.isSelected = false
-   //         cell.favButton.setImage(UIImage(named : "addFav100"), for: UIControlState.normal)
-            favClic?.backgroundColor = UIColor.green
-        }else {
-            print("second 02")
-  //          favClic?.isSelected = true
-      //      cell.favButton.setImage(UIImage(named : "favYellow96"), for: UIControlState.normal)
-     //      favClic?.backgroundColor = UIColor.red
-        }
-        */
-        
-  //      favClicked = cell.favButton
-  //      favClic?.setImage(UIImage(named : "addFav100"), for: UIControlState.normal)
-    //    favClic?.setImage(UIImage(named : "favYellow100"), for: UIControlState.normal)
-//        favClic?.addTarget(self, action: #selector(myButtonTapped), for: UIControlEvents.touchUpInside)
-   //     self.view.addSubview(favClic!)
- 
-   //     cell.favButton.setImage(UIImage(named: "addFav100"), for: .normal)
-  //      cell.favButton.setImage(UIImage(named: "FavYellow96"), for: .highlighted)
-
         let urlPost = post["link"] as! String
         urlPost1 = urlPost as String
-        
-  //      cell.titleLabel?.text = favResults[idx].title
         cell.titleLabel.text = post["title"] as? String
         titleShare = cell.titleLabel.text
-        
         let htmlTag = post["content"] as! String
         let content = htmlTag.replacingOccurrences(of: "<[^>]+>", with: "", options: .regularExpression, range: nil)
         cell.contentLabel.text = content
-
+        
+        //date format conversion
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy-MM-dd"
         let newDateFormatter = DateFormatter()
@@ -401,21 +271,16 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
             if urlYou != ""{
                 urlYoutube = String(urlYou)
                 print(urlYoutube)
-                cell.picMedia.isHidden = false
-         //       cell.labelMedia.isHidden = false
+                cell.picMedia.isHidden = false //icon for media files
             }
             else{
-                cell.picMedia.isHidden = true
-    //            cell.labelMedia.isHidden = true
-               // urlYou = ""
+                cell.picMedia.isHidden = true //icon for media files
             }
         }
-        
         do{
             let imgArray = (posts as AnyObject).value(forKey: "featured_image")
             let dataDic = imgArray as? [[String: Any]]
             self.imgPosts = dataDic!
-                
             let remoteImageUrlString = imgPosts[indexPath.row]
             let imageURL = remoteImageUrlString["source"] as? String
             //print(imageURL!)
@@ -423,60 +288,40 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
             
             let url = URL(string: imgURLShare!)
             cell.imagePost.sd_setImage(with: url, placeholderImage:nil, completed: { (image, error, cacheType, url) -> Void in
-                
-       //         let errorCode = (error! as NSError).code
-                
                 if ((error) != nil) {
-                    print("placeholder image ))))))))))))))")
+                    print("placeholder image...")
                     cell.imagePost.image = UIImage(named: "placeholderImage.png")
-                    
                 } else {
-                    print("(((((((((((((((((success use the image)")
+                    print("Success let using the image...")
                     cell.imagePost.sd_setImage(with: url)
                 }
             })
-            
-            
             if let imagePath = imageURL,
                 let imgUrl = URL(string:  imagePath){
-                cell.imagePost.image = UIImage(named: "loading4.jpg")
+                cell.imagePost.image = UIImage(named: "loading4.jpg") //image place
                 cell.imagePost.af_setImage(withURL: imgUrl)
-                
             }
-                
             else{
                 cell.imagePost.image = nil
             }
             imgShare = cell.imagePost.image
         }
-        
         cell.favButton.addTarget(self, action: #selector(ViewController.bookmarkTapped(_:)), for: .touchUpInside)
 
         return cell
     }
-    
- /*   func buttonTapped(_ sender:AnyObject) {
-        let buttonPosition:CGPoint = sender.convert(CGPoint.zero, to:self.tableView)
-        let indexPath = self.tableView.indexPathForRow(at: buttonPosition)
-    }*/
-    
     //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    
     // The cell calls this method when the user taps the heart button
     func PostsCellDidTapBookmark(_ sender: PostsCell) {
         guard let tappedIndexPath = tableView.indexPath(for: sender) else { return }
         print("Bookmark", sender, tappedIndexPath)
-        
-        // "Love" this item
-  /////      items[tappedIndexPath.row].love()
     }
     
     @objc func bookmarkTapped(_ sender: Any?) {
-        // We need to call the "love" method on the underlying object, but I don't know which row the user tapped!
+        // We need to call the method on the underlying object, but I don't know which row the user tapped!
         // The sender is the button itself, not the table view cell. One way to get the index path would be to ascend
         // the view hierarchy until we find the UITableviewCell instance.
         print("Bookmark Tapped", sender!)
-        
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -486,14 +331,13 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
        if segue.identifier == "ViewFav1" {
-            print("is true ++++++++++++++++++++++++++++++++")
+            print("Bookmarks View segue")
             let controller = segue.destination as! BookmarkViewController
-      //      controller.delegate = self
             controller.favoritePosts = favResults
             
        }
        else{
-            print("is false -------------------------------")
+            print("DetailsPost View segue")
             let cell = sender as! UITableViewCell
             let indexPath = tableView.indexPath(for: cell)
             let post = posts[(indexPath?.row)!]
@@ -505,13 +349,9 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
     
     @IBAction func btnSharePosts(_ sender: Any) {
-      //  let title = titleLabel.text
-    //    let title = PostKeys.title
         let title = titleShare
-       // let URl = post![PostKeys.link]
         let URl = urlPost1
         let image = imgShare
-        
         let vc = UIActivityViewController(activityItems: [title, URl, image], applicationActivities: [])
         if let popoverController = vc.popoverPresentationController{
             popoverController.sourceView = self.view
@@ -522,36 +362,16 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     //Storing app data
     func storeData(){
- /*
-        //     defaults?.set(favResults, forKey: "savedData")
-        UserDefaults.standard.set(favResults, forKey: "savedData")
- //       defaults?.synchronize()
-        UserDefaults.standard.synchronize()
-        print("data stored successfully")
-        print(favResults.count)
-        */
         let data = NSKeyedArchiver.archivedData(withRootObject: favResults)
         UserDefaults.standard.set(data, forKey: "savedData1")
     }
     
     //Getting app data
     func getData(){
-/*
-        let data = UserDefaults.standard.value(forKey: "savedData")
- //       let data = defaults?.value(forKey: "savedData")
-        if data != nil{
-            favResults = data as! [[String: Any]]
-            print("data nil")
-        }
-        else{
-            print("data not nil")
-        }
-    */
         let outData = UserDefaults.standard.data(forKey: "savedData1")
         if outData != nil{
         let dict = NSKeyedUnarchiver.unarchiveObject(with: outData!)as! [[String: Any]]
