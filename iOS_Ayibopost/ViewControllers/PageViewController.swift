01158fb6ee164235eaeb6c472b957e145b702c89
IsaSam <isaacsamuel27@gmail.com>
2019-07-14 18:39:47 -0400

    shareApp menu done

--- iOS_Ayibopost/ViewControllers/PageViewController.swift
+++ iOS_Ayibopost/ViewControllers/PageViewController.swift
@@ -15,7 +15,11 @@ class PageViewController: UIViewController, MFMailComposeViewControllerDelegate{
     @IBOutlet weak var contentLabel: UILabel!
     @IBOutlet weak var contactPage: UIView!
     @IBOutlet weak var sendEmail: UIButton!
-    @IBOutlet weak var appSoreBtn: UIButton!
+    @IBOutlet weak var sharePage: UIView!
+    @IBOutlet weak var imageLogo: UIImageView!
+    @IBOutlet weak var shareButton: UIButton!
+    @IBOutlet weak var appStoreBtn: UIButton!
+    @IBOutlet weak var contentShare: UIView!
     
     var postsPage: [[String: Any]] = []
     var categoryName: String?
@@ -30,17 +34,53 @@ class PageViewController: UIViewController, MFMailComposeViewControllerDelegate{
 
         print("1***********************************************")
         print(categoryName!)
+
+        contactPage.layer.cornerRadius = 12
+        contactPage.layer.borderWidth = 1
+        contactPage.layer.borderColor = UIColor.lightGray.cgColor
      
         if categoryName == "Contact"{
             pageID = "19399"
+            sharePage.isHidden = true
             sendEmail.isHidden = false
-            appSoreBtn.isHidden = true
+            appStoreBtn.isHidden = true
             PageLabel.text? = "CONTACT"
+            
+            sendEmail.layer.cornerRadius = 7
+            sendEmail.backgroundColor = .clear
+            sendEmail.layer.borderWidth = 1
+            sendEmail.layer.borderColor = UIColor.black.cgColor
+            
+        }
+        else if categoryName == "Partager"{
+            shareButton.backgroundColor = .clear
+            shareButton.layer.cornerRadius = 7
+            shareButton.layer.borderWidth = 1
+            shareButton.layer.borderColor = UIColor.black.cgColor
+            
+//            imageLogo.layer.cornerRadius = imageLogo.frame.height / 2
+  //          imageLogo.clipsToBounds = true
+            
+            imageLogo.backgroundColor = .clear
+            imageLogo.layer.cornerRadius = 10
+            imageLogo.layer.borderWidth = 1
+            imageLogo.layer.borderColor = UIColor.lightGray.cgColor
+            
+            pageID = ""
+            contentShare.isHidden = false
+            contentShare.layer.cornerRadius = 12
+            contentShare.layer.borderWidth = 1
+            contentShare.layer.borderColor = UIColor.lightGray.cgColor
+            contactPage.isHidden = true
+            PageLabel.text? = "PARTAGER À VOS AMIS"
+            
         }
         else if categoryName == "À propos"{
             pageID = "2"
+            sharePage.isHidden = true
             sendEmail.isHidden = true
-            appSoreBtn.isHidden = true
+            appStoreBtn.isHidden = true
+            contactPage.layer.borderColor = UIColor.clear.cgColor
             PageLabel.text? = "À PROPOS"
             contentLabel.text? = """
             Bien plus qu’un média en ligne, Ayibopost est une plateforme d’auto-defense intellectuelle. Informer, éduquer et sensibiliser : voici le triptyque que nous mettons en oeuvre, et ceci surtout lorsqu’il s’agit de vulgariser des informations ou concepts d’utilité publique. Nous sommes fiers de faire du journalisme explicatif.
@@ -56,8 +96,15 @@ class PageViewController: UIViewController, MFMailComposeViewControllerDelegate{
         }
         else if categoryName == "AppStore"{
             pageID = ""
+            
+            appStoreBtn.layer.cornerRadius = 7
+            appStoreBtn.backgroundColor = .clear
+            appStoreBtn.layer.borderWidth = 1
+            appStoreBtn.layer.borderColor = UIColor.black.cgColor
+            
+            sharePage.isHidden = true
             sendEmail.isHidden = true
-            appSoreBtn.isHidden = false
+            appStoreBtn.isHidden = false
             PageLabel.text? = "L'APPLICATION"
             contentLabel.text? = """
             Merci d’utiliser l'application AyiboPost! Pour vous offrir la meilleure expérience possible, nous mettons régulièrement l’application à jour dans l’App Store. Profitez des dernières améliorations en téléchargeant la version la plus récente !
@@ -71,6 +118,20 @@ class PageViewController: UIViewController, MFMailComposeViewControllerDelegate{
         topBarLogo()
    //     getContact()
     }
+    
+    @IBAction func shareButton(_ sender: Any) {
+        let title = "Je vous invite à utiliser l'application ayiboPOST"
+        let URl = "https://ayibopost.com"
+        let image = imageLogo.image
+        
+        let vc = UIActivityViewController(activityItems: [title, URl, image], applicationActivities: [])
+        if let popoverController = vc.popoverPresentationController{
+            popoverController.sourceView = self.view
+            popoverController.sourceRect = self.view.bounds
+        }
+        self.present(vc, animated: true, completion: nil)
+    }
+    
     
     func topBarLogo(){
         let logoContainer = UIView(frame: CGRect(x: 0, y: 0, width: 270, height: 30))
