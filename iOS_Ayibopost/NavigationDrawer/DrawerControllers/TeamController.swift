ef18b50bdf247221a50dddc795f682184c232c4c
IsaSam <isaacsamuel27@gmail.com>
2019-07-28 12:43:34 -0400

    cleaning codes

--- iOS_Ayibopost/NavigationDrawer/DrawerControllers/TeamController.swift
+++ iOS_Ayibopost/NavigationDrawer/DrawerControllers/TeamController.swift
@@ -85,9 +85,6 @@ class TeamController: UIViewController, UITableViewDataSource, UITableViewDelega
   tableView.dataSource = self
   
   fetchTeamID()
- // fetchTeam()
-//  fetchTeamNamePosts()
-//  loadMoreAuthors()
   self.navigationController?.navigationBar.isTranslucent = false
  }
  
@@ -103,7 +100,7 @@ class TeamController: UIViewController, UITableViewDataSource, UITableViewDelega
  }
  
  @objc func didPullToRefresh(_ refreshControl: UIRefreshControl){
-  fetchTeamNamePosts()
+  fetchTeamID()
  }
  
  @IBAction func actShowMenu(_ sender: Any) {
@@ -183,177 +180,12 @@ class TeamController: UIViewController, UITableViewDataSource, UITableViewDelega
   
  }
  
- /* 1111111
-  
-  private func fetchTeam(){
-  
-  let url = URL(string: "https://ayibopost.com/wp-json/wp/v2/pages/19405?&_embed")!
-  let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
-  let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
-  let task = session.dataTask(with: request) {(data, response, error) in
-  //-- This will run when the network request returns
-  if let error = error{
-       let errorAlertController = UIAlertController(title: "Cannot Get data Authors", message: "The Internet connections appears to be offline", preferredStyle: .alert)
-       let cancelAction = UIAlertAction(title: "Retry", style: .cancel)
-       errorAlertController.addAction(cancelAction)
-       self.present(errorAlertController, animated: true)
-       print(error.localizedDescription)
-  } else if let data = data,
-        let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]{
-   
-        let pageContent = dataDictionary["content"] as! Dictionary<String,AnyObject>
-        let htmlTag =  pageContent["rendered"] as! String
-        //let content = htmlTag.replacingOccurrences(of: "<[^>]+>", with: "", options: .regularExpression, range: nil)
-        //            self.contentLabel.text = content.stringByDecodingHTMLEntities
-      //  print(content)
-        print(htmlTag)
-   
-
-
-  
-  }
-  }
-  task.resume()
-  }
- */
- 
- 
- /*  private func fetchTeam(){
-  
-  let url = URL(string: "https://ayibopost.com/wp-json/wp/v2/pages/19405?&_embed")!
-  let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
-  let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
-  let task = session.dataTask(with: request) {(data, response, error) in
-  //-- This will run when the network request returns
-  if let error = error{
-  let errorAlertController = UIAlertController(title: "Cannot Get data Authors", message: "The Internet connections appears to be offline", preferredStyle: .alert)
-  let cancelAction = UIAlertAction(title: "Retry", style: .cancel)
-  errorAlertController.addAction(cancelAction)
-  self.present(errorAlertController, animated: true)
-  print(error.localizedDescription)
-  } else if let data = data,
-  let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]{
-  
-  let pageContent = dataDictionary["content"] as! Dictionary<String,AnyObject>
-  let htmlTag =  pageContent["rendered"] as! String
-  let content = htmlTag.replacingOccurrences(of: "<[^>]+>", with: "", options: .regularExpression, range: nil)
-  //            self.contentLabel.text = content.stringByDecodingHTMLEntities
-  print(content)
-  
-  }
-  }
-  task.resume()
-  }*/
- 
- 
- 
- func fetchTeamNamePosts(){
-  
-  self.activityIndicatory.startAnimating() //====================
-//  AyiboAPIManager.shared.get(url: "https://ayibopost.com/wp-json/posts?&filter[posts_per_page]=\(loadNumber)") { (result, error) in
-// AyiboAPIManager.shared.get(url: "https://ayibopost.com/wp-json/posts?page=\(loadNumber)") { (result, error) in
-//           AyiboAPIManager.shared.get(url: "https://ayibopost.com/wp-json/posts/") { (result, error) in
-  AyiboAPIManager.shared.get(url: "https://ayibopost.com/wp-json/posts?filter[category_name]=&filter[posts_per_page]=\(loadNumber)") { (result, error) in
-   
-   if error != nil{
-    let errorAlertController = UIAlertController(title: "Cannot Get Data", message: "The Internet connections appears to be offline", preferredStyle: .alert)
-    let cancelAction = UIAlertAction(title: "Retry", style: .cancel)
-    errorAlertController.addAction(cancelAction)
-    self.present(errorAlertController, animated: true)
-    return
-   }
-        if result != nil{
-            self.posts1 = result!
-            // print(self.posts)
-            }
-        else{ print("nil")}
-   
-   let author = (self.posts1 as AnyObject).value(forKey: "author")
-   let dataDicAuthor = author as? [[String: Any]]
-   
-   for data in dataDicAuthor!{
-
-    print("===========================\(data)")
-    let name = data["name"] as? String
-    
-    if self.authorArray1.contains(name!){
-    }else{
-     self.authorArray1.append(name!)
-     self.byName.append(data)
-    }
-    self.byName.reverse()
-    self.tableView.reloadData()
-   }
-   
-   self.tableView.reloadData() // to tell table about new data
-   //   self.activityIndicatory.stopAnimating() //====================
-  }
-  
- }
- 
- func loadMoreAuthors(){
-  print("load 01...")
- // loadNumber = loadNumber + 50
-  self.activityIndicatory.startAnimating() //====================
-  //         AyiboAPIManager.shared.get(url: "https://ayibopost.com/wp-json/posts?page=\(loadNumber)") { (result, error) in
-  AyiboAPIManager.shared.get(url: "https://ayibopost.com/wp-json/posts?filter[category_name]=&filter[posts_per_page]=\(loadNumber)") { (result, error) in
-// AyiboAPIManager.shared.get(url: "https://ayibopost.com/wp-json/posts?page=\(loadNumber)") { (result, error) in
-   if error != nil{
-    let errorAlertController = UIAlertController(title: "Cannot Get Data", message: "The Internet connections appears to be offline", preferredStyle: .alert)
-    let cancelAction = UIAlertAction(title: "Retry", style: .cancel)
-    errorAlertController.addAction(cancelAction)
-    self.present(errorAlertController, animated: true)
-    return
-   }
-   //  self.posts1 = result!
-   // print(self.posts)
-   //////
-   do{
-    for item in result!
-       {
-         self.posts1.append(item)
-       }
-    let author = (self.posts1 as AnyObject).value(forKey: "author")
-    let dataDicAuthor = author as? [[String: Any]]
-    
-    for data in dataDicAuthor!{
-     print("===========================\(data)")
-     let name = data["name"] as? String
-     
-     if self.authorArray1.contains(name!){
-     }else{
-      self.authorArray1.append(name!)
-      self.byName.append(data)
-
-     }
-    }
-    self.byName.reverse()
-    //print(result!)
-    self.tableView.reloadData()
-    
-   }
-   
-   
-   //////
-   
-//   self.tableView.reloadData() // to tell table about new data
-   //   self.activityIndicatory.stopAnimating() //====================
-  }
- }
- 
- /*
-  
-  }*/
  
  func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
   if indexPath.row + 1 == posts.count{
         print("load 1...")
-        //loadMoreAuthors()
-   
         fetchMoreTeamID()
-   
   }
-  //   fetchMoreTeamID()
  }
  
  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
