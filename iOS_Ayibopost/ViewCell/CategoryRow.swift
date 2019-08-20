0b944a3737f09dc11a71377cd19c7b8b26ce56bd
IsaSam <isaacsamuel27@gmail.com>
2019-08-20 08:51:29 -0400

    rename's

--- iOS_Ayibopost/ViewCell/CategoryRow.swift
+++ iOS_Ayibopost/ViewCell/CategoryRow.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class CategoryRow: UITableViewCell {
-
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -22,3 +22,29 @@ class CategoryRow: UITableViewCell {
     }
 
 }
+extension CategoryRow : UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "videoCell", for: indexPath as IndexPath)
+        return cell
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 12
+    }
+    
+}
+/*
+extension CategoryRow : UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+      //  let itemsPerRow:CGFloat = 4
+    //    let hardCodedPadding:CGFloat = 5
+     //   let itemWidth = (collectionView.bounds.width / itemsPerRow) - hardCodedPadding
+   //     let itemHeight = collectionView.bounds.height - (2 * hardCodedPadding)
+   //     return CGSize(width: itemWidth, height: itemHeight)
+        return CGSize(width: 30, height: 35)
+    }
+    
+}*/
+
